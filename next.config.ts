1722de1f4b22621a6740e8b20f661b0ad704bbd6
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 12:35:37 +0000

    For clinical microscopy https://www.news-medical.net/image-handler/pictu

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.news-medical.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
