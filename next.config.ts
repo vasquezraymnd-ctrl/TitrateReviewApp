6cc6d588cc3283b55f7074a27d8f01aa72fd4a61
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 12:37:20 +0000

    In clinical chemistry https://lms.smu.edu.ph/pluginfile.php/193798/cours

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lms.smu.edu.ph',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
