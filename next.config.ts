b32752c11e8de71a30b81876b3b9b367d2b87a90
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 12:32:58 +0000

    Add this in microbio https://upload.wikimedia.org/wikipedia/commons/thum

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
