6960717b933baa0c1faa89c83232759901c2f8ab
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 12:31:19 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://media.b

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.beckman.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
