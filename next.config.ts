8e90a1ce16d7c67ecc91e356290724726893449d
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 12:40:10 +0000

    For htmle https://patho.ch/media/bcvdk2ah/histopathologie.jpg

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -55,6 +56,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'www.sigmaaldrich.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'patho.ch',
         port: '',
         pathname: '/**',
       },
