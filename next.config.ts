6faffd92eb281c8b233c67372b5b9e731c6d26bf
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 12:38:59 +0000

    In htmle https://www.sigmaaldrich.com/deepweb/assets/sigmaaldrich/market

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.sigmaaldrich.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
